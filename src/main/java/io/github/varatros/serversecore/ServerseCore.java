27fdf5cda856bf8519244fa7a31dcb572705707b
BuildTools <unconfigured@null.spigotmc.org>
2022-02-14 13:58:38 -0500

    Shalom

--- src/main/java/io/github/varatros/serversecore/ServerseCore.java
+++ src/main/java/io/github/varatros/serversecore/ServerseCore.java
@@ -2,6 +2,8 @@ package io.github.varatros.serversecore;
 
 import io.github.varatros.serversecore.commands.MapCommand;
 import io.github.varatros.serversecore.commands.TwitchCommand;
+import io.github.varatros.serversecore.events.player.PlayerConnectionEvents;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ServerseCore extends JavaPlugin {
@@ -11,6 +13,9 @@ public final class ServerseCore extends JavaPlugin {
         // Plugin startup logic
         this.getCommand("map").setExecutor(new MapCommand());
         this.getCommand("twitch").setExecutor(new TwitchCommand());
+        Bukkit.getPluginManager().registerEvents(new PlayerConnectionEvents(), this);
+
+
 
     }
 
--- src/main/java/io/github/varatros/serversecore/events/player/PlayerConnectionEvents.java
+++ src/main/java/io/github/varatros/serversecore/events/player/PlayerConnectionEvents.java
@@ -1,15 +1,54 @@
 package io.github.varatros.serversecore.events.player;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import javax.naming.Name;
 
 public class PlayerConnectionEvents implements Listener {
 
+    final TextComponent joinPrefix = Component.text().content("[").color(NamedTextColor.GOLD)
+            .append(Component.text("→").color(NamedTextColor.GREEN))
+            .append(Component.text("] ").color(NamedTextColor.GOLD)).build();
+    final TextComponent quitPrefix = Component.text().content("[").color(NamedTextColor.GOLD)
+            .append(Component.text("←").color(NamedTextColor.RED))
+            .append(Component.text("] ").color(NamedTextColor.GOLD)).build();
     @EventHandler
     private void playerJoinEvent(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+        if (!player.hasPlayedBefore()) {
+            TextComponent welcomeMessage = Component.text().append(joinPrefix)
+                    .append(Component.text("Welcome ").color(NamedTextColor.GOLD))
+                    .append(Component.text(player.getName()).color(NamedTextColor.AQUA))
+                    .append(Component.text(" to the server for the very first time!")
+                            .color(NamedTextColor.GOLD))
+                    .build();
+            Bukkit.broadcast(welcomeMessage);
+            //Setup warp to spawn if needed.
+            return;
+        }
+        TextComponent joinMessage = Component.text().append(joinPrefix)
+                .append(Component.text(player.getName()).color(NamedTextColor.GOLD))
+                .append(Component.text(" joined this world.").color(NamedTextColor.GOLD))
+                .build();
+        Bukkit.broadcast(joinMessage);
+    }
+
+    @EventHandler
+    private void playerQuitEvent(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        TextComponent quitMessage = Component.text().append(quitPrefix)
+                .append(Component.text(player.getName()).color(NamedTextColor.GOLD))
+                .append(Component.text(" joined this world.").color(NamedTextColor.GOLD))
+                .build();
+        Bukkit.broadcast(quitMessage);
     }
 
 }
