3f4be4e57832e54189ea44b697d5d129d7ac4e3e
Varatros <unconfigured@null.spigotmc.org>
2022-02-15 09:19:18 -0500

    Shalom

--- src/main/java/io/github/varatros/serversecore/ServerseCore.java
+++ src/main/java/io/github/varatros/serversecore/ServerseCore.java
@@ -1,5 +1,6 @@
 package io.github.varatros.serversecore;
 
+import io.github.varatros.serversecore.commands.CommandSpyCommand;
 import io.github.varatros.serversecore.commands.MapCommand;
 import io.github.varatros.serversecore.commands.ShrugCommand;
 import io.github.varatros.serversecore.commands.TwitchCommand;
@@ -14,9 +15,13 @@ public final class ServerseCore extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        //Commands
         Objects.requireNonNull(this.getCommand("map")).setExecutor(new MapCommand());
         Objects.requireNonNull(this.getCommand("twitch")).setExecutor(new TwitchCommand());
         Objects.requireNonNull(this.getCommand("shrug")).setExecutor(new ShrugCommand());
+        Objects.requireNonNull(this.getCommand("commandspy")).setExecutor((new CommandSpyCommand()));
+
+        //Listeners
         Bukkit.getPluginManager().registerEvents(new PlayerConnectionEvents(), this);
 
 
--- src/main/java/io/github/varatros/serversecore/commands/CommandSpyCommand.java
+++ src/main/java/io/github/varatros/serversecore/commands/CommandSpyCommand.java
@@ -1,13 +1,28 @@
 package io.github.varatros.serversecore.commands;
 
+import io.github.varatros.serversecore.util.CommandSpyHandler;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class CommandSpyCommand implements CommandExecutor {
+
+    private final CommandSpyHandler commandSpyHandler = new CommandSpyHandler();
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player)) return false;
+        Player player = (Player) sender;
+        commandSpyHandler.toggleIsSpying(player.getUniqueId());
+        //Test, and make pretty.
+        if (commandSpyHandler.getSpying(player.getUniqueId())) {
+            player.sendMessage("Command Spy Enabled");
+        }
+        else {
+            player.sendMessage("Command Spy Disabled");
+        }
         return false;
     }
 }
