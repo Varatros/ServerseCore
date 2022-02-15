517bf7ff659e7a77740324397baaae24eb8327d1
Varatros <unconfigured@null.spigotmc.org>
2022-02-15 13:36:32 -0500

    Shalom

--- src/main/java/io/github/varatros/serversecore/events/player/PlayerChatEvents.java
+++ src/main/java/io/github/varatros/serversecore/events/player/PlayerChatEvents.java
@@ -1,26 +1,37 @@
 package io.github.varatros.serversecore.events.player;
 
 import io.github.varatros.serversecore.util.CommandSpyHandler;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
-import java.util.HashMap;
 import java.util.UUID;
 
 public class PlayerChatEvents implements Listener {
 
     private final CommandSpyHandler commandSpyHandler = new CommandSpyHandler();
+    final TextComponent prefix = Component.text().content("[").color(NamedTextColor.DARK_GRAY)
+            .append(Component.text(">").color(NamedTextColor.DARK_RED))
+            .append(Component.text("] ").color(NamedTextColor.DARK_GRAY)).build();
 
 
     @EventHandler
     private void playerCommandEvent(PlayerCommandPreprocessEvent event) {
         for (UUID playerUUID : commandSpyHandler.getSpying()) {
             Player player = Bukkit.getPlayer(playerUUID);
-            if (player != null) {
-                player.sendMessage(event.getPlayer()+": "+event.getMessage());
+                if (player != null && !player.hasPermission("serverse.staff")) {
+                    player.sendMessage(event.getPlayer().getName()+": "+event.getMessage());
+                    TextComponent commandSpyMessage = Component.text().append(prefix)
+                            .append(Component.text(event.getPlayer().getName()).color(NamedTextColor.GRAY))
+                            .append(Component.text(": ").color(NamedTextColor.DARK_GRAY))
+                            .append(Component.text(event.getMessage()).color(NamedTextColor.RED))
+                            .build();
+                    player.sendMessage(commandSpyMessage);
             }
         }
     }
